4ba975f6f99d75ecfea0118e6c254ed6b76cdbeb
Gideon Onyewuenyi <36352230+gideononyewuenyi@users.noreply.github.com>
2024-11-25 11:02:29 +0100

    Update settings.py

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l^gndqnd0y^yrzm3ju5b&$9m&wgraf0t3g6#-9gr$isi*1%#g3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['expense-hyh0htd0fkd5daay.canadacentral-01.azurewebsites.net']
 
 
 # Application definition
